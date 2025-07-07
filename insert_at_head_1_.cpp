d4a2287513a07401d46c7426600fe66ca8f59246
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-07 23:44:14 +0600

    Insert at head using Doubly Linked LIst

---
+++ insert_at_head_1_.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+
+class Node
+{
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+    
+};
+
+
+void print_forward(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insert_at_head(Node* &head, int val)
+{
+    Node* newnode = new Node(val);
+
+    newnode->next = head;
+    head->prev = newnode;
+    head = newnode;
+
+}
+
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+
+    a->next = b;
+    b->prev = a;
+
+
+    insert_at_head(head, 100);
+
+    print_forward(head);
+
+    return 0;
+}
