7c419b7e17e85b1155b38832d435d5bb15ccf440
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-08 01:56:23 +0600

    Delete at head

---
+++ delete_at_head_5.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+
+class Node
+{
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+    
+};
+
+
+void print_forward(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void delete_at_head(Node* &head, Node* tail)
+{
+    Node* deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+
+    if(head == NULL)
+    {
+        tail = NULL;
+        return;
+    }
+    head->prev = NULL;
+}
+ 
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+
+    a->next = tail;
+    tail->prev = a;
+
+
+    delete_at_head(head, tail); 
+    delete_at_head(head, tail);
+
+    print_forward(head);
+
+    return 0;
+}
